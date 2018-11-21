f90567b1c4ad2e07883edf9b80debedcced640ba
jaarieboii <jari.gantvoort97@gmail.com>
2018-11-21 13:56:40 +0100

    filmFactory
    Dummy Data created

--- database/factories/FilmFactory.php
+++ database/factories/FilmFactory.php
@@ -5,5 +5,9 @@ use Faker\Generator as Faker;
 $factory->define(App\film::class, function (Faker $faker) {
     return [
         //
+        'naam' => $faker->name,
+        'genres' => ['horror', 'action', 'sport', 'adventure'][rand(0,3)],
+        'lengte' => ['01:30','02:00','02:30','03:00'][rand(0,3)],
+        'cover_image'=>'noImage.png',
     ];
 });
--- resources/views/films/welcome.blade.php
+++ resources/views/films/welcome.blade.php
@@ -3,8 +3,15 @@
         <div class="row justify-content-center">
             <div class="col-md-12">
                 <div class="card">
-                    <div class="card-header">Filmprogramma</div>
-    
+                    <div class="card-header">
+                        <div class="row">
+                            <div class="col-md-4">Filmprogramma</div>
+                        
+                            <div class="col-md-8">
+                                Filter: 
+                            </div>
+                        </div>
+                    </div>
                     <div class="card-body">
                         <div class="row" >
                         @foreach ($films as $film)
