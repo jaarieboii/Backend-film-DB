29df5c68cdb2417665594f2e3cd3cadf72b0628d
jaarieboii <jari.gantvoort97@gmail.com>
2018-11-21 14:05:56 +0100

    filter class
    Builded first filterclass and abstract filterclass which i can extend

---
+++ app/Filters/AbstractFilter.php
@@ -0,0 +1,24 @@
+<?php
+
+// AbstractFilter.php
+
+namespace App\Filters;
+
+use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Builder;
+
+abstract class AbstractFilter
+{
+    protected $request;
+
+    protected $filters = [];
+
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+    public function filter(Builder $builder)
+    {   
+        
+    }
+}
---
+++ app/Filters/TypeFilter.php
@@ -0,0 +1,13 @@
+<?php
+
+
+
+namespace App\Filters;
+
+class TypeFilter
+{
+    public function filter()
+    {
+            
+    }
+}
