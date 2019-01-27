bd4ef7b2382be42c9bfec609fe4b2d1d3044b0a6
jaarieboii <jari.gantvoort97@gmail.com>
2019-01-27 18:17:01 +0100

    Update app.blade.php
    deleted

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -103,19 +103,6 @@
     
                                     </div>
                                 </li>
-                                <li class="nav-item dropdown">
-                                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                            Zalen <span class="caret"></span>
-                                        </a>
-        
-                                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                            <a class="dropdown-item" href="{{ url('zalen') }}">Zalen
-                                            </a>
-                                            <a class="dropdown-item" href="{{ url('zalen/create') }}">Create
-                                                </a>
-        
-                                        </div>
-                                    </li>
                             {{-- <li class="nav-item">
                                 <a class="nav-link" href="{{url('reserveringen')}}">Reserveringen</a>
                             </li> --}}
